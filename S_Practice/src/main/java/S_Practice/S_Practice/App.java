d31e6deb56890faa5fbab3b81cb5e19d99a5ef2a
kalagiri <kalagiri1@gmail.com>
2020-07-02 18:07:51 -0700

    First push to S_Practice

---
+++ S_Practice/src/main/java/S_Practice/S_Practice/App.java
@@ -0,0 +1,13 @@
+package S_Practice.S_Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ S_Practice/src/test/java/Reporting_Utils/Listeners.java
@@ -0,0 +1,93 @@
+package Reporting_Utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class Listeners extends TestListenerAdapter {
+
+	public ExtentReports extent;
+	public ExtentTest test;
+	public ExtentHtmlReporter htmlReporter;
+	public static WebDriver driver;
+
+	@Override
+	public void onStart(ITestContext testContext) {
+
+		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/Reports/Listeners_ExtentReports.html");
+		htmlReporter.config().setDocumentTitle("Automation Report");
+		htmlReporter.config().setReportName("Rest Assured API Report");
+		htmlReporter.config().setTheme(Theme.DARK);
+
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+		extent.setSystemInfo("Host name", "localhost");
+		extent.setSystemInfo("Environment", "QA");
+		extent.setSystemInfo("user", "Jp");
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		test = extent.createTest(result.getName());
+		test.log(Status.PASS, "Test Case Passed is: " + result.getName());
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File(
+				System.getProperty("user.dir") + "/ScreenShots_Pass" + result.getMethod().getMethodName() + ".png");
+
+		try {
+			FileHandler.copy(src, dest);
+			test.addScreenCaptureFromPath("./ScreenShots_Pass/" + result.getMethod().getMethodName() + '-' + ".png");
+		} catch (IOException e) {
+			System.out.println("could not write screenshot " + e.getMessage());
+		}
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		test = extent.createTest(result.getName());
+		test.log(Status.FAIL, "Test Case Failed is: " + result.getName());
+		test.log(Status.FAIL, "Test Case Failed is: " + result.getThrowable());
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File(
+				System.getProperty("user.dir") + "/ScreenShots_Fail" + result.getMethod().getMethodName() + ".png");
+
+		try {
+			FileHandler.copy(src, dest);
+			test.addScreenCaptureFromPath("./ScreenShots_Fail/" + result.getMethod().getMethodName() + '-' + ".png");
+		} catch (IOException e) {
+			System.out.println("could not write screenshot " + e.getMessage());
+		}
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		test = extent.createTest(result.getName());
+		test.log(Status.SKIP, "Test Case Skipped is: " + result.getName());
+
+	}
+
+	@Override
+	public void onFinish(ITestContext testContext) {
+		extent.flush();
+	}
+
+}
